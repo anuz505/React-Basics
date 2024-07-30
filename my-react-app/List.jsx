b735c2225e0f0030df95c8ac8505af259b41157c
anuj bhandari <anujb364@gmail.com>
2024-07-30 20:17:06 +0545

    rendering list using default parameters instead of default props function

--- my-react-app/List.jsx
+++ my-react-app/List.jsx
@@ -1,30 +1,38 @@
-import propTypes from "prop-types"
-function List(props){
-    let itemsList = [...props.items];
-     // albhabetic sorting
-     itemsList.sort((a,b) => a.name.localeCompare(b.name))
-    //numeric sorting
-    itemsList.sort((a,b)=> a.cal - b.cal)
-    const Listitems = itemsList.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; {fruit.cal}</li>)
-    const lowcalorie_fruits = props.items.filter(fruit => fruit.cal < 100)
-    const lowcalorie = lowcalorie_fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; {fruit.cal}</li>)
+import PropTypes from "prop-types";
+
+function List({ items = [], category = "Fruits" }) {
+    let itemsList = [...items];
+
+    // Combined sorting: first by name, then by calories
+    itemsList.sort((a, b) => {
+        const nameComparison = a.name.localeCompare(b.name);
+        if (nameComparison !== 0) return nameComparison;
+        return a.cal - b.cal;
+    });
+
+    const Listitems = itemsList.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; {fruit.cal}</li>);
+    const lowcalorie_fruits = items.filter(fruit => fruit.cal < 100);
+    const lowcalorie = lowcalorie_fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; {fruit.cal}</li>);
+
     return (
         <>
-            <h1>{props.category}</h1>
+            <h1>{category}</h1>
             <ol>{Listitems}</ol>
             <h1>Low Calorie Fruits</h1>
             <ul>{lowcalorie}</ul>
         </>
-    )
+    );
 }
 
 List.propTypes = {
-    category  : propTypes.string,
-    items : propTypes.arrayOf(propTypes.shape({id:propTypes.number, name: propTypes.string, cal: propTypes.number}))
-}
-List.defaultProps= {
-    items:[],
-    category: "Category not given"
-    
-} 
-export default List
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            cal: PropTypes.number.isRequired
+        })
+    ).isRequired,
+    category: PropTypes.string
+};
+
+export default List;
