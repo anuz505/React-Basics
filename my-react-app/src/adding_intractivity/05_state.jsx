b1e823af321d142c3c26737d1a5c232fc5e73f8e
anuj bhandari <anujb364@gmail.com>
2024-08-04 20:51:25 +0545

    multiple states

--- my-react-app/src/adding_intractivity/05_state.jsx
+++ my-react-app/src/adding_intractivity/05_state.jsx
@@ -3,6 +3,7 @@ import {sculptureList} from "./05_data.js";
 
 export default function Gallery(){
     const [index, setIndex]  = useState(0)
+    const [showmore,setShowmore] = useState(false)
     
     const handleindex = () => {
         if(index >= sculptureList.length -1){
@@ -12,13 +13,18 @@ export default function Gallery(){
             setIndex(index + 1)
         }
     }
+    const handleShowmore = ()=>{
+        setShowmore(!showmore)
+    }
     let sculture = sculptureList[index]
     return(
         <div>
             <h1>{sculture.name}</h1>
             <h2>{sculture.artist}</h2>
-            <p>{sculture.description}</p>
+            <button onClick={handleShowmore}>{showmore ? "Hide" : "More"}</button>
+            {showmore && <p>{sculture.description}</p>}
             <img src={sculture.url} alt={sculture.alt} />
+            
             <h2>{index} of {sculptureList.length}</h2>
             <button onClick={handleindex}>Next</button>
         </div>
