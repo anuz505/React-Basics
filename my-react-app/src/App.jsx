260451a82a08c0fd46c352e99c57b35fa085b4ea
anuj bhandari <anujb364@gmail.com>
2024-08-26 21:01:55 +0545

    simple use Ref

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
 import "../src/index.css";
 import ComponentA from "./03_managing_state/usecontext/ComponentA.jsx";
-
+import TodoList from "./02_adding_intractivity/12_TodoList.jsx";
 function App() {
   return (
     <>
-      <ComponentA />
+      <TodoList />
     </>
   );
 }
